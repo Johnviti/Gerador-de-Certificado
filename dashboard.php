7e85823d253c62e89d02c9289785cf0a023b24e0
Johnviti <tecnologiaamorim99@gmail.com>
2025-04-02 17:15:35 -0300

    Fix:  adjust message error

--- dashboard.php
+++ dashboard.php
@@ -231,7 +231,20 @@ $nome_usuario = $stmt->fetchColumn();
 
       <main class="content-area">
         <?php if (!empty($message)): ?>
-          <div class="alert-message bg-red-50 text-red-700 p-4 rounded-lg mb-6">
+          <?php 
+            // Determine if the message is an error or success
+            $isError = strpos(strtolower($message), 'erro') !== false;
+            $alertClass = $isError ? 'bg-red-50 text-red-700' : 'bg-green-50 text-green-700';
+            $iconClass = $isError ? 'text-red-500' : 'text-green-500';
+          ?>
+          <div class="alert-message <?php echo $alertClass; ?> p-4 rounded-lg mb-6 flex items-center">
+            <svg class="w-5 h-5 mr-3 <?php echo $iconClass; ?>" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+              <?php if ($isError): ?>
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+              <?php else: ?>
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+              <?php endif; ?>
+            </svg>
             <?php echo htmlspecialchars($message); ?>
           </div>
         <?php endif; ?>
